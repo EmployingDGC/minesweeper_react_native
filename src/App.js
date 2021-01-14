5a639607d585a32fbedad3fc1ae307e65efc1e54
EmployingDGC <daviguizani@gmail.com>
2021-01-14 08:02:07 -0300

    Starting from 0
    Starting from 0 again

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { StatusBar, View } from 'react-native';
 
-import Header from "./components/Header";
-import Main from "./components/Main";
-
 import styles from './styles';
 
 class App extends React.Component {
@@ -14,12 +11,6 @@ class App extends React.Component {
         this.state = {
             // umaPropriedade: this.props.umaPropriedade,
         }
-
-        this.properts = {
-            mines: 10,
-            rows: 10,
-            columns: 10,
-        }
     }
 
     render() {
@@ -28,16 +19,6 @@ class App extends React.Component {
                 <StatusBar
                     barStyle='light-content'
                     backgroundColor='#000000'
-                />
-
-                <Header
-                    mines={this.properts.mines}
-                />
-
-                <Main
-                    row={this.properts.rows}
-                    column={this.properts.columns}
-                    mines={this.properts.mines}
                 />
             </View>
         )
